a59c65d64ce16663eb127885d24d9dbad6555bee
Super3DSpace <jannisbrill@gmx.de>
2019-10-10 20:52:34 +0200

    Bullets added
    -Added Bullets
    -Playerbullets are moving from left to right
    -Playerbullets ignore walls

---
+++ Swamp Witch/Assets/TheGame/Scripts/SpawnBulletController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnBulletController : MonoBehaviour
+{
+    public GameObject bullet;
+    public float interval = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("shootBullet", interval, interval);
+    }
+    void shootBullet()
+    {
+        GameObject g = (GameObject)Instantiate(bullet, transform.position, Quaternion.identity);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
